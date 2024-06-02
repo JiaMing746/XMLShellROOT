174593f89cb7ef0d001ac6ebcd13d5f71f4f97d9
JiaMing746 <3961215806@qq.com>
2024-06-02 18:47:20 +0800

    Add files via upload

--- app/src/main/java/com/projectkr/shell/MainActivity.kt
+++ app/src/main/java/com/projectkr/shell/MainActivity.kt
@@ -9,90 +9,44 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.util.Base64
-import android.content.SharedPreferences
+import android.provider.Settings
 import android.util.DisplayMetrics
-import android.content.Context
-import android.net.ConnectivityManager
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
 import com.omarea.common.shared.FilePathResolver
+import com.omarea.common.ui.DialogHelper
 import com.omarea.common.ui.ProgressBarDialog
 import com.omarea.krscript.config.PageConfigReader
 import com.omarea.krscript.config.PageConfigSh
-import com.projectkr.shell.FloatMonitor
 import com.omarea.krscript.model.*
 import com.omarea.krscript.ui.ActionListFragment
 import com.omarea.krscript.ui.ParamsFileChooserRender
+import com.omarea.vtools.FloatMonitor
+import com.projectkr.shell.permissions.CheckRootStatus
 import com.projectkr.shell.ui.TabIconHelper
 import kotlinx.android.synthetic.main.activity_main.*
-import com.projectkr.shell.permissions.CheckRootStatus
-import com.omarea.common.ui.DialogHelper
-import android.view.LayoutInflater
-import android.widget.CompoundButton
-import android.provider.Settings
-import net.khirr.android.privacypolicy.PrivacyPolicyDialog
-import com.projectkr.shell.Update
 
 class MainActivity : AppCompatActivity() {
     private val progressBarDialog = ProgressBarDialog(this)
     private var handler = Handler()
-    private lateinit var globalSPF: SharedPreferences
     private var krScriptConfig = KrScriptConfig()
+
     private fun checkPermission(permission: String): Boolean = PermissionChecker.checkSelfPermission(this, permission) == PermissionChecker.PERMISSION_GRANTED
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-
-        //val signCode = String(Base64.decode("你的签名的base64，把SHA1转base64", Base64.DEFAULT))
-        //val signCheck = SignCheck(this, signCode)
-        Update().checkUpdate(this)
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-    if (connectivityManager?.activeNetworkInfo != null) {
-        if (connectivityManager.activeNetworkInfo.isConnected) {
-            Toast.makeText(this, "欢迎(无话可说)", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "无网络连接，自己写吧", Toast.LENGTH_SHORT).show()
-        }
-    } else {
-        Toast.makeText(this, "无法获取网络信息，自己写吧", Toast.LENGTH_SHORT).show()
-    }
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ThemeModeState.switchTheme(this)
         setContentView(R.layout.activity_main)
-        
-        val dialog = PrivacyPolicyDialog(this, getString(R.string.termsOfServiceUrl), getString(R.string.privacyPolicyUrl))
-        dialog.onClickListener = object : PrivacyPolicyDialog.OnClickListener {
-            override fun onAccept(isFirstTime: Boolean) {
-                Log.e("MainActivity", "Policies accepted")
-            }
 
-            override fun onCancel() {
-                Log.e("MainActivity", "Policies not accepted")
-                finish()
-                 }
-             }
-        dialog.title = getString(R.string.termsOfServiceTitle)
-        dialog.termsOfServiceSubtitle = getString(R.string.termsOfServiceSubtitle)
-        dialog.addPoliceLine(getString(R.string.PoliceLine1))
-        dialog.addPoliceLine(getString(R.string.PoliceLine2))
-        dialog.cancelText = getString(R.string.dialog_cancelText)
-        dialog.acceptText = getString(R.string.dialog_acceptText)
-        dialog.acceptButtonColor = ContextCompat.getColor(this, R.color.colorAccent)
-        dialog.europeOnly = false
-        dialog.show()
-        
         //supportActionBar!!.elevation = 0f
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
@@ -138,7 +92,6 @@ class MainActivity : AppCompatActivity() {
             }
         }).start()
 
-
         if (CheckRootStatus.lastCheckResult && krScriptConfig.allowHomePage) {
             val home = FragmentHome()
             val fragmentManager = supportFragmentManager
@@ -147,9 +100,8 @@ class MainActivity : AppCompatActivity() {
             transaction.commitAllowingStateLoss()
         }
 
-
         if (!(checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE) && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE))) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), 111);
         }
     }
 
@@ -216,7 +168,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-      
             override fun addToFavorites(clickableNode: ClickableNode, addToFavoritesHandler: KrScriptActionHandler.AddToFavoritesHandler) {
                 val page = if (clickableNode is PageNode) {
                     clickableNode
@@ -267,7 +218,7 @@ class MainActivity : AppCompatActivity() {
     private fun chooseFilePath(fileSelectedInterface: ParamsFileChooserRender.FileSelectedInterface): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, getString(R.string.kr_write_external_storage), Toast.LENGTH_LONG).show()
-            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2)
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2);
             return false
         } else {
             return try {
@@ -275,18 +226,18 @@ class MainActivity : AppCompatActivity() {
                 if (suffix != null && suffix.isNotEmpty()) {
                     chooseFilePath(suffix)
                 } else {
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    val intent = Intent(Intent.ACTION_GET_CONTENT);
                     val mimeType = fileSelectedInterface.mimeType()
                     if (mimeType != null) {
                         intent.type = mimeType
                     } else {
                         intent.type = "*/*"
                     }
-                    intent.addCategory(Intent.CATEGORY_OPENABLE)
-                    startActivityForResult(intent, ACTION_FILE_PATH_CHOOSER)
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    startActivityForResult(intent, ACTION_FILE_PATH_CHOOSER);
                 }
                 this.fileSelectedInterface = fileSelectedInterface
-                true
+                true;
             } catch (ex: java.lang.Exception) {
                 false
             }
@@ -337,49 +288,5 @@ class MainActivity : AppCompatActivity() {
         menu.findItem(R.id.action_graph).isVisible = (main_tabhost_cpu.visibility == View.VISIBLE)
 
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            when (item.itemId) {
-            R.id.option_menu_info -> {
-                val intent = Intent()
-                intent.setClass(this,AboutActivity::class.java)
-                startActivity(intent)            
-            }
-            R.id.option_menu_reboot -> {
-                DialogPower(this).showPowerMenu()
-            }
-            R.id.action_graph -> {
-                if (FloatMonitor.isShown == true) {
-                    FloatMonitor(this).hidePopupWindow()
-                    return false
-                }
-                if (Build.VERSION.SDK_INT >= 23) {
-                    if (Settings.canDrawOverlays(this)) {
-                        FloatMonitor(this).showPopupWindow()
-                        Toast.makeText(this, getString(R.string.float_monitor_tips), Toast.LENGTH_LONG).show()
-                    } else {
-                        //若没有权限，提示获取
-                        //val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                        //startActivity(intent);
-                        val intent = Intent()
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        intent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
-                        intent.data = Uri.fromParts("package", this.packageName, null)
-
-                        Toast.makeText(applicationContext, getString(R.string.permission_float), Toast.LENGTH_LONG).show()
-
-                        try {
-                            startActivity(intent)
-                        } catch (ex: Exception) {
-                        }
-                    }
-                } else {
-                    FloatMonitor(this).showPopupWindow()
-                    Toast.makeText(this, getString(R.string.float_monitor_tips), Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
